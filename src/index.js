875e4d88f1b96436c08eaf30ada8aee2d3658bfe
Shaqqour <mshaqqourm@gmail.com>
2020-03-06 15:29:04 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   // your code here
-// });
+document.addEventListener("DOMContentLoaded", () => {
 
-document.getElementById("submit").addEventListener("click", function(e) {
-  e.preventDefault();
-  let li = document.createElement("li");
-  li.innerText = document.getElementById("new-task-description").value;
-  console.log(document.getElementById("new-task-description").value);
-  let ul = document.getElementById("tasks");
-  ul.appendChild(li);
-  //ul.appendChild(li);
+  const newTaskForm = document.getElementById("create-task-form");
+
+  newTaskForm.addEventListener("submit", createNewTask);
+
 });
+
+function createNewTask(event) {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+
+};
+
+function appendNewTask(task) {
+  document.getElementById("tasks").appendChild(task);
+};
+
+
+
+// document.getElementById("submit").addEventListener("click", function(e) {
+//   e.preventDefault();
+//   let li = document.createElement("li");
+//   li.innerText = document.getElementById("new-task-description").value;
+//   console.log(document.getElementById("new-task-description").value);
+//   let ul = document.getElementById("tasks");
+//   ul.appendChild(li);
+//   //ul.appendChild(li);
+// });
