6936fe0ef8ea1b31646a59d86b8a2e779c44d086
Shaqqour <mshaqqourm@gmail.com>
2020-03-06 14:44:14 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+document.getElementById("submit").addEventListener("click", function(e) {
+  e.preventDefault();
+  let li = document.createElement("li");
+  li.innerText = document.getElementById("new-task-description").value;
+  console.log(document.getElementById("new-task-description").value);
+  let ul = document.getElementById("tasks");
+  ul.appendChild(li);
+  //ul.appendChild(li);
 });
