f1c0f9c63243d443881c9b46a2ebfe44260a2a4c
kseggelke918 <katie.seggelke@gmail.com>
2020-03-07 15:25:50 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskDescription = document.getElementById('new-task-description');
+
+  const newTask = document.getElementById('tasks');
+
+
+
 });
+
+
+// description box: id - new-task-description
+// submit button = type - submit 
+// after submit - go to ul with id = "tasks"
