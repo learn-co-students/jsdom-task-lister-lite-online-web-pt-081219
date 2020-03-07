878121eeb80e58392ff9caa9f1ec6d864cfa48b6
Christian Cain <christian22cain@yahoo.com>
2020-03-07 13:55:35 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let formElement = document.querySelector('#create-task-form');
+  let inputElement = document.querySelector('#new-task-description');
+  let ulElement = document.querySelector('#list ul')
+
+  formElement.addEventListener('submit', function(e){
+    e.preventDefault();
+    let value = inputElement.value;
+
+    let liElement = document.createElement('li');
+
+    let deleteElement = document.createElement('button');
+    deleteElement.innerText = 'X';
+    deleteElement.addEventListener('click', function(e){
+      e.target.parentNode.remove();
+    })
+
+    liElement.innerHTML = value;
+    liElement.append(deleteElement);
+    ulElement.append(liElement);
+  });
+
 });
