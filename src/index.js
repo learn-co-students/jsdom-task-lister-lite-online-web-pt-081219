2e76e8264fc3e5afacd160f4fb205bc05ea20404
github username <github email address>
2020-03-08 11:53:38 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -6,19 +6,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const list = document.querySelector('div#list ul#tasks');
 
 
+  
+
   submit.addEventListener('click', function(event) {
       event.preventDefault();
       const task = form.querySelector('input#new-task-description').value;
-    document.querySelector('ul#tasks').innerHTML += `<li id="${task}">${task} <button id="${task}">X</button></li>`;
+    document.querySelector('ul#tasks').innerHTML += `<li id="${task}">${task} <button id="${task}" onClick="deleteMe()">X</button></li>`;
   });
   
-
-  // list.addEventListener('click', (delete) {
-  //     if delete.target.nodeName === 'BUTTON'){
-  //         const deleteMe = delete.querySelector('li').id
-  //         list.removeChild(list.querySelector(`li#${deleteMe}`));
-  //     }
-  // });
-
+  function deleteMe() {
+    let deleteId = list.querySelector(`li#${event.srcElement.id}`);
+    list.removeChild(deleteId);
+  };
+    
 
 });
