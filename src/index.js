99fc4c1b9b0309252bc24d6188bdf8a5e8860371
kseggelke918 <katie.seggelke@gmail.com>
2020-03-09 15:48:05 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form');
   const taskDescription = document.getElementById('new-task-description');
-
   const newTask = document.getElementById('tasks');
 
+  taskForm.addEventListener('submit', createNewTask);
 
+  function createNewTask() {
+    event.preventDefault();
+    const newTaskItem = document.createElement("li");
+    
+    newTaskItem.innerText = taskDescription.value;
 
-// description box: id - new-task-description
-// submit button = type - submit 
-// after submit - go to ul with id = "tasks"
+    newTask.appendChild(newTaskItem);
+    event.target.reset()
+  }  
+
+})
