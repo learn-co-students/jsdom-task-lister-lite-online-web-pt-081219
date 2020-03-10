ffad6583f000940f1c28e9217ec7f3d1c68c6e67
Leah Silber <LeahSilber@gmail.com>
2020-03-09 20:48:22 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.querySelector('#create-task-form');
+const submission = document.querySelector('#new-task-description');
+const list = document.querySelector('#tasks');
+
+form.addEventListener("submit", event => {
+  event.preventDefault()
+  list.innerHTML += `<li>${submission.value}</li>`;
+  form.reset()
 });
