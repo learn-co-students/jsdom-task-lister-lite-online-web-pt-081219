cf14c7e85a4037f9a8ca7aa67fd03aef419b4973
Alec Hall <alecnhall@gmail.com>
2020-03-10 18:28:23 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById('create-task-form');
+const submitted_task = document.getElementById('new-task-description');
+const list = document.getElementById('tasks')
+
+form.addEventListener("submit", function(e) {
+  event.preventDefault();
+  list.innerHTML += `<li> ${submitted_task.value} <a id="delete-task">x</a> </li>`;
+  form.reset();
 });
+
+
+
+
+
+
