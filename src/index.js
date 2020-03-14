55aef98bc3726f1ef30fd51d7d3de2005e7b2997
James <jamescscalise@gmail.com>
2020-03-14 15:11:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let comment_form = document.getElementById('create-task-form');
+  let comment_section = document.getElementById('tasks');
+
+
+  comment_form.addEventListener('submit', (event)=>{
+    let new_comment = document.createElement('li');
+    new_comment.innerText= document.getElementById('new-task-description').value;
+    comment_section.appendChild(new_comment);
+    event.preventDefault();
+  })
 });
