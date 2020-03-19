bb8a2ac53c623200d716bf35cf8afa3214cee079
sydnee510 <sbsydnee161416@gmail.com>
2020-03-18 22:58:55 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let newForm = document.querySelector('#create-task-form');
+  let newTask = document.querySelector('#new-task-description');
+	let allTasks = document.querySelector('#tasks');
+
+newForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  allTasks.innerHTML += `<li>${newTask.value}</li>`;
+  newForm.reset()
+})
 });
