9c6d2465d337c0689e9b013d080c224bb75f6e34
kierra <51520037+kierrakay@users.noreply.github.com>
2020-04-27 16:07:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -24,6 +24,30 @@
 //   event.target.reset();
 // };
 
+                        //  task written below could be named anything it's just hold the value of task created.
 // const appendNewTask = task => {
 //   document.getElementById("tasks").appendChild(task);
 // };
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskUl = document.getElementById("tasks");
+
+ newTaskForm.addEventListener("submit", createNewTask);
+});
+
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+}
+
+const appendNewTask = e => {
+  document.getElementById("tasks").appendChild(e);
+}
